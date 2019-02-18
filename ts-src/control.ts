ecdfa9458a3b8c8b3eb24d5b918af9c8e0ac3872
Kristóf <z.kristof97@gmail.com>
2019-02-18 12:21:55 +0100

    finished

---
+++ ts-src/control.ts
@@ -0,0 +1,66 @@
+import * as PIXI from 'pixi.js';
+import Application from "./application";
+import Animation from './animation';
+
+export default class Control {
+
+    public static enable(app: PIXI.Application): void {
+        window.onkeydown = (e: KeyboardEvent) => {
+            Control.keyDownHandler(e, app);
+        };
+
+        window.onkeyup = (e: KeyboardEvent) => {
+            Control.keyUpHandler(e);
+        };
+    }
+
+    public static disable(): void {
+        window.onkeydown = null;
+        window.onkeyup = null;
+    }
+
+    private static keyUpHandler(e: KeyboardEvent): void {
+        switch (e.key) {
+            case ' ':
+                Application.canShoot = true;
+                break;
+            case 'ArrowLeft':
+                Application.player.velocityX = 0;
+                break;
+            case 'ArrowUp':
+                Application.player.velocityY = 0;
+                break;
+            case 'ArrowRight':
+                Application.player.velocityX = 0;
+                break;
+            case 'ArrowDown':
+                Application.player.velocityY = 0;
+                break;
+        }
+    }
+
+    private static keyDownHandler(e: KeyboardEvent, app: PIXI.Application): void {
+        let speed = 5;
+        switch (e.key) {
+            case ' ':
+                //we use a flag to prevent user from shooting a lot of missles while holding down the spacebar
+                if (Application.canShoot === true) {
+                    Animation.missle(app);
+                    Application.canShoot = false;
+                }
+                break;
+            case 'ArrowLeft':
+                Application.player.velocityX = -speed;
+                break;
+            case 'ArrowUp':
+                Application.player.velocityY = -speed;
+                break;
+            case 'ArrowRight':
+                Application.player.velocityX = speed;
+                break;
+            case 'ArrowDown':
+                Application.player.velocityY = speed;
+                break;
+        }
+    }
+}
--- ts-src/game.ts
+++ ts-src/game.ts
@@ -1,12 +1,12 @@
 import * as PIXI from 'pixi.js';
 import Application from './application';
-import Animation from './animation';
 import Menu from './menu';
 import Panel from './panel';
 import GameOver from './gameOver';
 import GamePlay from './gamePlay';
-import HitTest from './hitTest';
 import Sounds from './sound';
+import Movement from './movement';
+import Control from './control';
 
 let appOptions: PIXI.ApplicationOptions = {
     width: 800,
@@ -81,7 +81,7 @@ function gameLoop(): void {
     Sounds.stopSounds();
     Sounds.playSounds();
 
-    movements();
+    Movement.start(app);
 
     if (Application.state === 'menu') {
         Menu.init(app);
@@ -94,7 +94,7 @@ function gameLoop(): void {
 
         Application.state = 'play';
     } else if (Application.state === 'play') {
-        addControl();
+        Control.enable(app);
 
         GamePlay.initPlay(app);
 
@@ -140,8 +140,8 @@ function gameLoop(): void {
             Application.state = 'menu';
         }
     } else if (Application.state === 'gameOver') {
-        removeControl();
-        
+        Control.disable();
+
         GameOver.display(app);
 
         Panel.deactivatePauseBtn();
@@ -153,123 +153,5 @@ function gameLoop(): void {
         window.location.replace('https://www.google.com/');
 
         Application.state = '';
-    }
-}
-
-function addControl(): void {
-    window.addEventListener('keydown', keyDownHandler);
-    window.addEventListener('keyup', keyUpHandler);
-}
-
-function removeControl(): void {
-    window.removeEventListener('keydown', keyDownHandler);
-    window.removeEventListener('keyup', keyUpHandler);
-}
-
-function movements(): void {
-    if (Application.movementOn === true) {
-        backgroundMovement();
-        playerMovement();
-        enemyMovement();
-        missleMovement();
-    }
-}
-
-function backgroundMovement(): void {
-    //parallax scrolling
-    for (let i = 0; i < GamePlay.parallaxImgs.length; i++) {
-        GamePlay.parallaxImgs[i].tilePosition.x -= GamePlay.parallaxImgs[i].velocity;
-    }
-}
-
-function playerMovement(): void {
-    Application.player.x += Application.player.velocityX;
-    Application.player.y += Application.player.velocityY;
-
-    //we check if the player is inside the bounds, e.g. did not leave the screen
-    GamePlay.checkPosition(5, 4, app);
-}
-
-function enemyMovement(): void {
-    for (let i = Application.enemies.length - 1; i >= 0; i--) {
-        let currentEnemy: PIXI.Sprite = Application.enemies[i];
-
-        currentEnemy.x -= 4;
-
-        //we check if the enemy has left the screen, if so remove it
-        if (currentEnemy.x <= 0 - currentEnemy.width / 2) {
-            Application.enemies = Application.enemies.filter(e => e !== currentEnemy);
-            app.stage.removeChild(currentEnemy);
-        }
-        
-        HitTest.detectCollision(Application.player, currentEnemy, app);
-    }
-}
-
-function missleMovement(): void {
-    for (let i = Application.missles.length - 1; i >= 0; i--) {
-        let currentMissle: PIXI.Sprite = Application.missles[i];
-
-        currentMissle.x += 7;
-
-        //If missle has left the screen, remove it
-        if (currentMissle.x > app.view.width) {
-            Application.missles = Application.missles.filter(m => m !== currentMissle);
-            app.stage.removeChild(currentMissle);
-        }
-
-        //check to see if missle hit any of the enemies 
-        for (let j = 0; j < Application.enemies.length; j++) {
-            let currentEnemy: PIXI.Sprite = Application.enemies[j];
-
-            if (currentMissle !== null && currentMissle !== undefined && currentEnemy !== null && currentEnemy !== undefined) {
-                GamePlay.checkTargetHit(currentMissle, currentEnemy, app);
-            }
-        }
-    }
-}
-
-function keyUpHandler(e: KeyboardEvent): void {
-    switch (e.key) {
-        case ' ':
-            Application.canShoot = true;
-            break;
-        case 'ArrowLeft':
-            Application.player.velocityX = 0;
-            break;
-        case 'ArrowUp':
-            Application.player.velocityY = 0;
-            break;
-        case 'ArrowRight':
-            Application.player.velocityX = 0;
-            break;
-        case 'ArrowDown':
-            Application.player.velocityY = 0;
-            break;
-    }
-}
-
-function keyDownHandler(e: KeyboardEvent): void {
-    let speed = 5;
-    switch (e.key) {
-        case ' ':
-        //we use a flag to prevent user from shooting a lot of missles while holding down the spacebar
-            if (Application.canShoot === true) {
-                Animation.missle(app);
-                Application.canShoot = false;
-            }
-            break;
-        case 'ArrowLeft':
-            Application.player.velocityX = -speed;
-            break;
-        case 'ArrowUp':
-            Application.player.velocityY = -speed;
-            break;
-        case 'ArrowRight':
-            Application.player.velocityX = speed;
-            break;
-        case 'ArrowDown':
-            Application.player.velocityY = speed;
-            break;
     }
 }
